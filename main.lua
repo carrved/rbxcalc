de68b5609715a5aa2e409905734601dc4803359f
z4xi <36055611+z4xi@users.noreply.github.com>
2022-08-13 02:00:40 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,17 @@
 -- made by z4#4332's monkey brain
+-- https://github.com/z4xi/rbxcalc
+
+print([[	
+ ____   ____   __ __     __   ____  _         __ 
+|    \ |    \ |  |  |   /  ] /    || |       /  ]
+|  D  )|  o  )|  |  |  /  / |  o  || |      /  / 
+|    / |     ||_   _| /  /  |     || |___  /  /  
+|    \ |  O  ||     |/   \_ |  _  ||     |/   \_ 
+|  .  \|     ||  |  |\     ||  |  ||     |\     |
+|__|\_||_____||__|__| \____||__|__||_____| \____|          
+
+	   https://github.com/z4xi/rbxcalc
+]])
 
 -- Instances:
 
@@ -198,7 +211,7 @@ Go.TextWrapped = true
 
 -- Scripts:
 
-local function MDEMZS_fake_script() -- Base.Calculator 
+local function ISLQ_fake_script() -- Base.Calculator 
 	local script = Instance.new('Script', Base)
 
 	-- main / global vars
@@ -315,5 +328,49 @@ local function MDEMZS_fake_script() -- Base.Calculator
 		add1 = nil
 		add2 = nil
 	end)
+	
+	
+	------ draggable ------
+	
+	
+	local UserInputService = game:GetService("UserInputService")
+	
+	local gui = script.Parent
+	
+	local dragging
+	local dragInput
+	local dragStart
+	local startPos
+	
+	local function update(input)
+		local delta = input.Position - dragStart
+		gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+	end
+	
+	gui.InputBegan:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+			dragging = true
+			dragStart = input.Position
+			startPos = gui.Position
+	
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragging = false
+				end
+			end)
+		end
+	end)
+	
+	gui.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			dragInput = input
+		end
+	end)
+	
+	UserInputService.InputChanged:Connect(function(input)
+		if input == dragInput and dragging then
+			update(input)
+		end
+	end)
 end
-coroutine.wrap(MDEMZS_fake_script)()
+coroutine.wrap(ISLQ_fake_script)()
